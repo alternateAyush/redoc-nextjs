77b8697974db5dd6053cc15031ba3a17b9dbdb5a
Ayush Bhatt <ayushbhatt2002@gmail.com>
2024-11-06 14:33:36 +0530

    no navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,32 +3,32 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata = {
-  title: "OpenAI Redoc",
-  description: "OpenAI openAPI sepcs by Redocly",
+    title: "OpenAI Redoc",
+    description: "OpenAI openAPI sepcs by Redocly",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[rgba(48,60,76,0.8)]`}
-      >
-        <Navbar/>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[rgba(48,60,76,0.8)]`}
+            >
+                {/* <Navbar/> */}
+                {children}
+            </body>
+        </html>
+    );
 }
 
 //
